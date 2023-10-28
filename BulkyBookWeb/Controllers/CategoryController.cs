af0c6217682682a36b71d8e36a35cdd2e511ba6d
shaheerqureshi <shaheerqureshi334@gmail.com>
2023-10-28 08:55:57 -0400

    Edit and Delete Added

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -40,5 +40,69 @@ namespace BulkyBookWeb.Controllers
             }
             return View(obj);
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoryFromDbFirst = _db.Categories.FirstOrDefault(x=>x.Id==id);
+            //var categoryFromDbSingle = _db.Categories.SingleOrDefault(x=>x.Id==id);
+            if(categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Name Cannot be same as Display Order");
+            }
+            if (ModelState.IsValid)
+            {
+
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryToDeleteFromDb = _db.Categories.Find(id);
+            //var categoryFromDbFirst = _db.Categories.FirstOrDefault(x=>x.Id==id);
+            //var categoryFromDbSingle = _db.Categories.SingleOrDefault(x=>x.Id==id);
+            if (categoryToDeleteFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryToDeleteFromDb);
+        }
+
+        //Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            
+                var obj = _db.Categories.Find(id);
+                _db.Categories.Remove(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            
+        }
     }
 }
