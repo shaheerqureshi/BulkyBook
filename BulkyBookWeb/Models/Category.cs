4336f1f0e081b13d5b8cba75dd4411923bb3cc74
shaheerqureshi <shaheerqureshi334@gmail.com>
2023-10-28 05:02:19 -0400

    Client Side Validations Added

--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyBookWeb.Models
 {
@@ -7,7 +8,10 @@ namespace BulkyBookWeb.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [DisplayName("Name")]
         public string name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100, ErrorMessage = "Order Must be between 1 and 100 Only")]
         public int DisplayOrder { get; set; }
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
